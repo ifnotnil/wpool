1bca4dbc1920341b75a048c20ea21bbc8a8eab5a
Kostas Stamatakis <moukoublen@gmail.com>
2025-03-13 18:31:44 +0200

    ci bench

--- scripts/go.mk
+++ scripts/go.mk
@@ -30,3 +30,7 @@ test-n-read: test
 .PHONY: bench
 bench: # runs all benchmarks
 	go test -benchmem -run=^Benchmark$$ -mod=readonly -bench=. ./...
+
+.PHONY: ci-bench
+ci-bench: # runs all benchmarks
+	go test -benchtime=1s -count=7 -benchmem -run=^Benchmark$ -mod=readonly -bench=. ./...
